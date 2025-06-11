a6aeab95ab172c28a404ddaf861610f7868fd130
mertsadekist <mert.sadek@istrealestate.com>
2025-06-11 17:15:40 +0400

    Update manage_developers_projects.php

--- manage_developers_projects.php
+++ manage_developers_projects.php
@@ -1,15 +1,7 @@
 <?php
 // manage_developers_projects.php
 
-$host = 'localhost';
-$db = 'u891594679_stoks';
-$user = 'u891594679_stoks';
-$pass = '^1QQHgpeQ7o';
-
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once 'db_connection.php';
 
 // Add Developer
 if (isset($_POST['add_developer'])) {
