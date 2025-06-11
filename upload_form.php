4f47c0e3f2f88ba66ca121c4f7bd449aa96a43e5
mertsadekist <mert.sadek@istrealestate.com>
2025-06-11 16:52:40 +0400

    Update upload_form.php

--- upload_form.php
+++ upload_form.php
@@ -2,10 +2,10 @@
 // upload_form.php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
+if (!isset($_SESSION['user_id'])) {␊
+    header("Location: index.php");
+    exit();␊
+}␊
 
 require_once 'db_connection.php';
 $developers = $conn->query("SELECT id, name FROM developers ORDER BY name");
