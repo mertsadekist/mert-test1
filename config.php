bcfb87e7f5c272eb42812271f0db500799968d4d
mertsadekist <mert.sadek@istrealestate.com>
2025-06-11 16:03:12 +0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php - Database configuration
+
+// Update these values with your database credentials
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'u891594679_stoks');
+define('DB_USER', 'u891594679_stoks');
+define('DB_PASS', '^1QQHgpeQ7o');
+?>
