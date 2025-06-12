4b5adae466b147bf34a72d278495302f12a6bfe0
mertsadekist <mert.sadek@istrealestate.com>
2025-06-12 10:07:06 +0400

    Update upload_form.php

--- upload_form.php
+++ upload_form.php
@@ -2,10 +2,10 @@
 // upload_form.php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {␊
+if (!isset($_SESSION['user_id'])) {
     header("Location: index.php");
-    exit();␊
-}␊
+    exit();
+}
 
 require_once 'db_connection.php';
 require_once 'csrf.php';
